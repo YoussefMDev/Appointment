237d10b70e3704add3f0946b0ca26b1aca55bda1
yousef <mobasheryousef4@gmail.com>
2026-05-22 15:09:09 +0300

    Debug: expose stack trace in error middleware

--- middlewares/error.js
+++ middlewares/error.js
@@ -14,7 +14,7 @@ const handleValidationErrorDB = err => {
 const sendErrorDev = (err, res) => res.status(err.statusCode).json({ status: err.status, error: err, message: err.message, stack: err.stack });
 const sendErrorProd = (err, res) => {
   if (err.isOperational) {
-    res.status(err.statusCode).json({ status: err.status, message: err.message });
+    res.status(err.statusCode).json({ status: err.status, message: err.message, stack: err.stack });
   } else {
     console.error('ERROR 💥', err);
     res.status(500).json({ status: 'error', message: 'Something went very wrong!' });
